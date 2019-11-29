6b4502238870e7322c54abb5e032777d1676fc75
Sohail Khan <57131474+sohailahmedmengal@users.noreply.github.com>
2019-11-29 12:56:24 +0500

    Update my2ndprog.c

--- my2ndprog.c
+++ my2ndprog.c
@@ -1,1 +1,32 @@
-# C-programs
+int main()
+{
+    int n, t1 = 0, t2 = 1, nextTerm = 0;
+    printf("Enter the number of terms");
+    scanf("%d" ,&n);
+    printf("Fibonacci Series");
+
+    for (int i = 1; i <= n; ++i)
+    {
+        // Prints the first two terms.
+        if(i == 1)
+        {
+            scanf ("%d" ,&t1);
+
+            continue;
+        }
+        if(i == 2)
+        {
+            scanf ("%d" ,&t2);
+
+            continue;
+        }
+        nextTerm = t1 + t2;
+        t1 = t2;
+        t2 = nextTerm;
+
+
+        printf ("nextTerm");
+
+    return 0;
+    }
+}
